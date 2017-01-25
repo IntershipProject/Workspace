eae8c0971063cfc1dd49318903569e7a40396d84
bahadircolak44 <bahdrcolak@gmail.com>
2017-01-25 14:25:57 +0100

    Database Update

--- src/DatabaseHandler.java
+++ src/DatabaseHandler.java
@@ -3,6 +3,8 @@ import java.net.UnknownHostException;
 import java.util.*;
 import java.util.logging.Level;
 
+import org.bson.Document;
+
 import com.mongodb.DB;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
@@ -11,6 +13,7 @@ import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.util.JSON;
 
@@ -18,13 +21,14 @@ import com.mongodb.util.JSON;
 public class DatabaseHandler {
 
 	private DB db;
-	private DBCollection dbCollection;
-
+	private MongoCollection dbCollection;
+	private DBCollection coll;
 	public DatabaseHandler() {
 		MongoClient mongoClient = null;
         try {
             mongoClient = new MongoClient("localhost", 27017);
-            MongoDatabase db = mongoClient.getDatabase("census");
+            MongoDatabase db = mongoClient.getDatabase("testDB");
+            dbCollection=  db.getCollection("myCollection");
             
             
         } catch (Exception e) {
@@ -35,17 +39,26 @@ public class DatabaseHandler {
 
 	public void insertElement(String sourceIP, String destinationIP, String description, int sourcePort,
 			int destinationPort) {
-		BasicDBObject dbObject = new BasicDBObject();
+		Document dbObject = new Document();
 		dbObject.put("Source IP", sourceIP);
 		dbObject.put("Destination IP", destinationIP);
 		dbObject.put("Description", description);
 		dbObject.put("Source Port", sourcePort);
 		dbObject.put("Destination Port", destinationPort);
-		dbCollection.insert(dbObject);
+		dbCollection.insertOne(dbObject);
 		
 	}
 	public void findElement(String element){
-		dbCollection.getCollection(element);
+		DBCursor cursor = coll.find();
+        int i = 1;
+			
+        while (cursor.hasNext()) { 
+        	System.out.println("++++++++++++++++++++++++++++++++++++++");
+           System.out.println("Inserted Document: "+i); 
+           System.out.println(cursor.next()); 
+           System.out.println("++++++++++++++++++++++++++++++++++++++");
+           i++;
+        }
 	}
 
 }
