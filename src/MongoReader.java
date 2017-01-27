0826916704c4f9305e44dcf1644fd5622ba4ef3d
MBerkYuksel <mehmetberk.yuksel@outlook.com>
2017-01-27 12:10:45 +0100

    Database reader is updated.
    Json format is unnecessary. Bson format had already been used in
    MongoCollection.

--- src/MongoReader.java
+++ src/MongoReader.java
@@ -1,52 +1,29 @@
 
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.ServerAddress;
-
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
-
 import org.bson.Document;
 import java.util.Arrays;
-import com.mongodb.Block;
-
-import com.mongodb.client.MongoCursor;
-import static com.mongodb.client.model.Filters.*;
-import com.mongodb.client.result.DeleteResult;
-import static com.mongodb.client.model.Updates.*;
-import com.mongodb.client.result.UpdateResult;
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class MongoReader {
 	private static MongoDatabase database;
 	private static MongoCollection<Document> collection;
 	private static MongoClient mongoClient;
-	
-	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		mongoClient = new MongoClient("localhost", 27017);
 		database = mongoClient.getDatabase("testDB");
 		collection = database.getCollection("myCollection");
-		System.out.println(collection.find().first().toJson());
-		System.exit(0);
+
+		//
+		System.out.println(collection.find().first().getString("Source IP"));
+
+		//
 		for (Document cur : collection.find()) {
-		    System.out.println(cur.toJson());
+			System.out.println(cur);
+			System.exit(0);
 		}
 
 	}
-	private static void createDoc(Document doc) {
-		// TODO Auto-generated method stub
-		doc = new Document("name", "MongoDB")
-                .append("type", "database")
-                .append("count", 1)
-                .append("versions", Arrays.asList("v3.2", "v3.0", "v2.6"))
-                .append("info", new Document("x", 203).append("y", 102));
-
-	}
-
 }
-
